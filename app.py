f44cc71bf1fb516ca7a7bbed2bbc0582f057a610
Brian Dumont <dumont.brian@outlook.com>
2018-12-05 18:02:03 -0500

    updated print statment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/',methods=['POST'])
 def foo():
    data = json.loads(request.data)
-   print ("New commit by: {}".format(data['commits'][0]['author']['name']))
+   print (data)
    return ("OK")
 
 if __name__ == '__main__':
