12616ca5bf931ba12f5c62afb2999b6831913b8a
Brian Dumont <dumont.brian@outlook.com>
2018-12-05 19:47:32 -0500

    Remove Pyautogui

--- addToPatchSelenium.py
+++ addToPatchSelenium.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.common.exceptions import TimeoutException
 import time
-import pyautogui
 import os
 import sys
 import googleExport
