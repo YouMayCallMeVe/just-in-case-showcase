06201b39c1e6ed9d61e9c7322199cf3e7a75ec79
Brian Dumont <dumont.brian@outlook.com>
2018-12-06 21:25:33 +0000

    Added download functionality

--- addToPatchSelenium.py
+++ addToPatchSelenium.py
@@ -51,10 +51,18 @@ def waitForLoadXPATHHidden(path, x, driver):
 #print(googleExport.getValues())
 def run(items):
   options = Options()
+  prefs = {'download.default_directory': "/home/bdumont/just-in-case-showcase",'download.prompt_for_download': False,'download.directory_upgrade': True,'safebrowsing.enabled': False,'safebrowsing.disable_download_protection': True}
+  options.add_experimental_option('prefs', prefs)
   options.add_argument('--headless')
   options.add_argument('--disable-popup-blocking')
   options.add_argument('--disable-gpu')
   driver = webdriver.Chrome("/home/bdumont/just-in-case-showcase/chromedriver", chrome_options=options)
+  driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+  params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': "/home/bdumont/just-in-case-showcase"}}  
+  command_result = driver.execute("send_command", params)
+  print("response from browser:")
+  for key in command_result: 
+    print("result:" + key + ":" + str(command_result[key]))
   driver.get('https://mcriss-cdsdev.appiancloud.com/suite/design/lQBY7b8XyEG3sHPiDxlu8ZrV8LENAU_SQ5oJCUwg_freLRs2IulV4fLo--rXPlSQnPvVn5G5ay5rLhFVaH_juNa3rlpvXP4eG-1dzZZJpGoHs2L5LQ')
   username = driver.find_element_by_id('un')
   username.send_keys("bdumont@chenega.com")
