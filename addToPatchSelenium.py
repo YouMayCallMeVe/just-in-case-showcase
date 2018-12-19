6aa5109fa2e1fb6eff67f635ce3e3551fc4b8893
Dumont <BDumont@chenega.com>
2018-12-19 15:06:22 -0500

    Updating for linux

--- addToPatchSelenium.py
+++ addToPatchSelenium.py
@@ -54,14 +54,14 @@ def run(items, issue, email):
   
   options = Options()
   #prefs = {'download.default_directory': "C:/Users/bdumont/Desktop/git repos/just-in-case-showcase",'download.prompt_for_download': False,'download.directory_upgrade': True,'safebrowsing.enabled': False,'safebrowsing.disable_download_protection': True}
-  options.add_experimental_option('prefs', prefs)
+  #options.add_experimental_option('prefs', prefs)
   options.add_argument('--headless')
   options.add_argument('--disable-popup-blocking')
   options.add_argument('--disable-gpu')
   driver = webdriver.Chrome("chromedriver", chrome_options=options)
   driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
   #params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': "C:/Users/bdumont/Desktop/git repos/just-in-case-showcase"}}  
-  command_result = driver.execute("send_command", params)
+  #command_result = driver.execute("send_command", params)
   print("response from browser:")
   for key in command_result: 
     print("result:" + key + ":" + str(command_result[key]))
